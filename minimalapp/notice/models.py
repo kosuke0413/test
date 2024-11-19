750a4c4cad86e42055d4ce3ba6f58285ce0e0035
MASA1051999 <knz2374416@stu.o-hara.ac.jp>
2024-11-19 10:59:25 +0900

    返信テーブル作成

--- minimalapp/notice/models.py
+++ minimalapp/notice/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-
+# お知らせテーブル
 class Notice(db.Model):
     __tablename__ = "notice"
     notice_id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +8,12 @@ class Notice(db.Model):
     content = db.Column(db.Text, nullable=False)
     image = db.Column(db.LargeBinary, nullable=True)
     tag = db.Column(db.String(50), nullable=True)
+
+
+# お知らせ返信テーブル
+class NoticeReply(db.Model):
+    __tablename__ = "reply"
+    notice_id = db.Column(db.Integer, nullable=False) 
+    reply_id = db.Column(db.Integer, primary_key=True)    
+    name = db.Column(db.String(10), nullable=False)       
+    content = db.Column(db.Text, nullable=False)
