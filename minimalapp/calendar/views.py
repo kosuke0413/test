d5427985d3bc0f5c4caa3d5ff634adeb25f9605b
koreoko <knz2374537@stu.o-hara.ac.jp>
2024-11-19 15:11:41 +0900

    カレンダー表示（未完成）

--- minimalapp/calendar/views.py
+++ minimalapp/calendar/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint,render_template
+from datetime import datetime
+import calendar
 
 #Blueprintでcalendarアプリを生成する
 calendar = Blueprint(
@@ -8,6 +10,15 @@ calendar = Blueprint(
     static_folder="../static"
 )
 
-@calendar.route("/")
+@calendar.route('/')
 def index():
-    return render_template("calendar.html")
+    # 現在の年と月を取得
+    now = datetime.now()
+    year = now.year
+    month = now.month
+
+    # カレンダーを作成
+    cal = calendar.Calendar(firstweekday=6)
+    month_days = cal.monthdayscalendar(year, month)
+
+    return render_template('calendar.html', year=year, month=month, month_days=month_days)
