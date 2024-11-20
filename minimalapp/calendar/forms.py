d4f8daee2c9c62d7f73bb76582473d39d3566fce
koreoko <knz2374537@stu.o-hara.ac.jp>
2024-11-20 11:45:24 +0900

    カレンダー登録Viewsの作成

---
+++ minimalapp/calendar/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from flask_wtf.form import FlaskForm
+from wtforms.fields.simple import SubmitField
+from wtforms import StringField,TextAreaField,DateField
+from wtforms.validators import DataRequired,length
+
+class CalendarregistForm(FlaskForm):
+
+    title = StringField(
+        "タイトル",
+        validators=[
+            DataRequired(message="タイトルは必須です。"),
+            length(max=30,message="30文字以内で入力してください。"),
+        ]
+    )
+
+     text = TextAreaField(
+        "本文",
+        validators=[
+            DataRequired(message="本文は必須です。"),
+            length(max=200,message="200文字以内で入力してください。"),
+        ]     
+    )
+
+
+    date = DateField(
+       "日付",
+       validators=[
+           DataRequired(message="日付は必須です。"),
+       ]
+    )
+
+    submit = SubmitField("登録")
--- minimalapp/calendar/views.py
+++ minimalapp/calendar/views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint,render_template
 from datetime import datetime
 import calendar
+from minimalapp.calendar.forms import CalendarregistForm
+from minimalapp.calendar.models import Calendar
+from app import db
 
 #Blueprintでcalendarアプリを生成する
 Calendar = Blueprint(
@@ -22,3 +25,23 @@ def index():
     month_days = cal.monthdayscalendar(year, month)
 
     return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
+
+#カレンダー登録のエンドポイント
+@Calendar.route('/register', methods=["GET","POST"])
+def setEvent():
+    #カレンダーフォームをインスタンス化
+    form = CalendarregistForm()
+
+    if form.validate_on_submit():
+        calen = Calendar(
+            event_title = form.title.data,
+            content = form.text.data,
+            day = form.date.data
+        )
+
+    db.session.add(calen)
+    db.session.commit()
+    return "登録完了"
+
+return render_template("calendar/register.html")
+
