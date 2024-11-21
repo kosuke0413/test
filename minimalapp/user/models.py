c84e63023178ced43ebf9105b97f7a1f70385ddf
maemura589 <knz2374523@stu.o-hara.ac.jp>
2024-11-21 13:47:14 +0900

    userのmodels修正

--- minimalapp/user/models.py
+++ minimalapp/user/models.py
@@ -1,9 +1,9 @@
 from flask_login import UserMixin
 from werkzeug.security import check_password, generate_password
-
+ 
 from app import db
-
-
+ 
+ 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
     local_id = db.Column(db.String(3),nullable=True)
@@ -11,17 +11,17 @@ class User(db.Model, UserMixin):
     name = db.Column(db.String(10),nullable=True)
     mailaddress = db.Column(db.String(50),nullable=True)
     password = db.Column(db.String(50),nullable=True)
-
+ 
     @property
     def pasword(self):
         raise AttributeError("読み取り不可")
-    
+   
     @password.setter
     def pasword(self, password):
-        self.password = generate_password(password)
-
+        self.pasword = generate_password(password)
+ 
     def verify_pasword(self, password):
-        return check_password(self.password, password)
-
-    def is_duplicate_mailaddress(self):
-        return User.query.filter_by(mailaddress=self.mailaddress).first() is not None
+        return check_password(self.pasword, password)
+ 
+    def is_duplicate_mailadores(self):
+        return User.query.filter_by(mailaddoress=self.mailaddoress).first() is not None
