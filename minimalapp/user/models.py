5e4b6c749ec34e7cc3d3d5ce3e3936fc589d820b
kazura1423 <knz2374559@stu.o-hara.ac.jp>
2024-11-26 11:16:18 +0900

    ユーザーテーブル変更

--- minimalapp/user/models.py
+++ minimalapp/user/models.py
@@ -1,26 +1,35 @@
 from flask_login import UserMixin
- 
-from app import db
- 
+from app import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
  
 class User(db.Model, UserMixin):
     __tablename__ = "users"
-    local_id = db.Column(db.String(3),nullable=True)
+    local_id = db.Column(db.String(3))
     user_id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.String(10),nullable=True)
-    mailaddress = db.Column(db.String(50),nullable=True)
-    password = db.Column(db.String(50),nullable=True)
- 
+    name = db.Column(db.String(10))
+    mailaddress = db.Column(db.String(50))
+    password_hash = db.Column(db.String)
+
+
+    # パスワードを読み取り不可に設定
     @property
     def password(self):
         raise AttributeError("読み取り不可")
-   
-    # @password.setter
-    # def password(self, password):
-    #     self.password = generate_password(password)
- 
-    # def verify_password(self, password):
-    #     return check_password(self.password, password)
- 
-    def is_duplicate_mailadores(self):
-        return User.query.filter_by(mailaddoress=self.mailaddress).first() is not None
+    
+    # パスワードのセッター
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # パスワードのチェック(ハッシュ値の一致確認)
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    # メッセージアドレス重複チェック
+    def is_duplicate_mailaddress(self):
+        return User.query.filter_by(mailaddress=self.mailaddress).first() is not None
+    
+# ログインしているユーザー情報を取得する関数を作成する
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
