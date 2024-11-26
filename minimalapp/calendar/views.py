f0b5daf6d8bc5ac73f6f11e89c525ce7755ffa85
koreoko <knz2374537@stu.o-hara.ac.jp>
2024-11-26 10:02:06 +0900

    カレンダーviewsの修正

--- minimalapp/calendar/views.py
+++ minimalapp/calendar/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint,render_template,redirect,url_for,flash
 from datetime import datetime
 import calendar
 from minimalapp.calendar.forms import CalendarregistForm
@@ -26,156 +26,13 @@ def index():
     cal = calendar.Calendar(firstweekday=6)
     month_days = cal.monthdayscalendar(year, month)
 
-from flask import Blueprint,render_template,redirect,url_for,flash
-from datetime import datetime
-import calendar
-from minimalapp.calendar.forms import CalendarregistForm
-from minimalapp.calendar.models import Calendar
-from app import db
-
-#Blueprintでcalendarアプリを生成する
-Calen = Blueprint(
-    "calendar",
-    __name__,
-    template_folder="templates",
-    static_folder="../static"
-)
-
-#現在の年月のカレンダーを作成
-@Calen.route('/')
-def index():
-    # 現在の年と月を取得
-    now = datetime.now()
-    year = now.year
-    month = now.month
-
-    # カレンダーを作成
-    cal = calendar.Calendar(firstweekday=6)
-    month_days = cal.monthdayscalendar(year, month)
-
     return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
-
-
-#次の月のカレンダーを作成
-@Calen.route('/next<int:year>/<int:month>')
-def nextindex(year, month):
-    
-    #12月の次を1月にして、年に1を足す
-    if month == 12:
-        year = year + 1
-        month = 1
-    else:
-        year = year
-        month = month + 1
-
-    # カレンダーを作成
-    cal = calendar.Calendar(firstweekday=6)
-    month_days = cal.monthdayscalendar(year, month)
-
-    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
-
-#先月のカレンダーを作成
-@Calen.route('/before/<int:year>/<int:month>')
-def beforeindex(year, month):
-    #12月の次を1月にして、年に1を引く
-    if month == 1:
-        year = year - 1
-        month = 12
-    else:
-        year = year
-        month = month - 1
-
-    # カレンダーを作成
-    cal = calendar.Calendar(firstweekday=6)
-    month_days = cal.monthdayscalendar(year, month)
-
-    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
-
-
-#カレンダー登録のエンドポイント
-@Calen.route('/register', methods=["GET", "POST"])
-def setEvent():
-    #カレンダーフォームをインスタンス化
-    form = CalendarregistForm()
-
-    if form.validate_on_submit():
-        calen = Calendar(
-            event_title=form.title.data,
-            content=form.text.data,
-            day=form.date.data,
-            local_id="abc"
-        )
-
-        db.session.add(calen)
-        db.session.commit()
-        return redirect(url_for("calendar.index"))
-
-    return render_template("calendar/register.html", form=form)
-
-@Calen.route("/event_edit/<calendar_id>", methods=["GET", "POST"])
-def edit_event(calendar_id):
-    form = CalendarregistForm()
-    event = Calendar.query.filter_by(calendar_id=calendar_id).first()
-
-    if form.validate_on_submit():
-        event.event_title = form.title.data,
-        event.content = form.text.data,
-        event.day = form.date.data,
-        event.local_id = "abc"
-
-
-        db.session.commit()
-        return redirect(url_for("calendar.index"))
-    
-     # フォームの初期値に投稿データをセット
-    form.title.data = event.event_title
-    form.text.data = event.content
-    form.date.data = event.day
-
-    return render_template("calendar/event_edit.html", form=form,event=event)
-
-
-#カレンダーイベント削除のエンドポイント
-@Calen.route('/<int:calendar_id>/delete', methods=['POST'])
-def delete(calendar_id):
-    # 削除処理
-    calen = Calendar.query.get(calendar_id)
-    if calen:
-        db.session.delete(calen)
-        db.session.commit()
-    flash('イベントを削除しました')
-    return redirect(url_for('calendar.index'))  # 削除が完了するとtopページに遷移
-
-
-
-# 選択された日のイベント一覧
-@Calen.route('/events/<date>')
-def events(date):
-    from datetime import datetime
-    selected_date = datetime.strptime(date, "%Y-%m-%d").date()
-    events = Calendar.query.filter_by(date=selected_date).all()
-    return render_template('event_list.html', date=selected_date, events=events)
-
-#選択された年月のカレンダーを表示
-@Calen.route('/select/<int:year>/<int:month>')
-def selectindex(year, month):
-    year = year
-    month = month
-
-
-    # カレンダーを作成
-    cal = calendar.Calendar(firstweekday=6)
-    month_days = cal.monthdayscalendar(year, month)
-
-    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
-    return render_template('calendar/calendar.html', year=year, month=month,
-                           month_days=month_days)
 
 
 # 次の月のカレンダーを作成
 @Calen.route('/next<int:year>/<int:month>')
 def nextindex(year, month):
-
+    
     # 12月の次を1月にして、年に1を足す
     if month == 12:
         year = year + 1
@@ -188,8 +45,7 @@ def nextindex(year, month):
     cal = calendar.Calendar(firstweekday=6)
     month_days = cal.monthdayscalendar(year, month)
 
-    return render_template('calendar/calendar.html', year=year, month=month,
-                           month_days=month_days)
+    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
 
 
 # 先月のカレンダーを作成
@@ -207,8 +63,7 @@ def beforeindex(year, month):
     cal = calendar.Calendar(firstweekday=6)
     month_days = cal.monthdayscalendar(year, month)
 
-    return render_template('calendar/calendar.html', year=year, month=month,
-                           month_days=month_days)
+    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
 
 
 # カレンダー登録のエンドポイント
@@ -227,35 +82,63 @@ def setEvent():
 
         db.session.add(calen)
         db.session.commit()
-        return redirect(url_for("calendar/calendar"))
+        return redirect(url_for("calendar.index"))
 
     return render_template("calendar/register.html", form=form)
 
-# @Calen.route("/event_edit/<calendar_id>", methods=["GET", "POST"])
-# def edit_event(calendar_id):
-#     form = CalendarregistForm():
 
-#     event = Calendar.query.filter_by(id=calendar_id).first()
+# カレンダー編集のエンドポイント
+@Calen.route("/event_edit/<calendar_id>", methods=["GET", "POST"])
+def edit_event(calendar_id):
+    form = CalendarregistForm()
+    event = Calendar.query.get_or_404(calendar_id)
+    if form.validate_on_submit():
+        event.event_title = form.title.data,
+        event.content = form.text.data,
+        event.day = form.date.data,
+        event.local_id = "abc"
 
-#     if form.validate_on_submit():
-#         calen = Calendar(
-#             event.event_title=form.title.data,
-#             content=form.text.data,
-#             day=form.date.data,
-#             local_id="abc"
-#         )
+        db.session.commit()
+        return redirect(url_for("calendar.index"))
+    
+    # フォームの初期値に投稿データをセット
+    form.title.data = event.event_title
+    form.text.data = event.content
+    form.date.data = event.day
 
-#         db.session.add(calen)
-#         db.session.commit()
-#         return "登録完了"
+    return render_template("calendar/event_edit.html", form=form,event=event)
 
-#     return render_template("calendar/calendar.html", form=form)
+
+# カレンダーイベント削除のエンドポイント
+@Calen.route('/<int:calendar_id>/delete', methods=['POST'])
+def delete(calendar_id):
+    # 削除処理
+    calen = Calendar.query.get(calendar_id)
+    if calen:
+        db.session.delete(calen)
+        db.session.commit()
+    flash('イベントを削除しました')
+    return redirect(url_for('calendar.index'))  # 削除が完了するとtopページに遷移
 
 
 # 選択された日のイベント一覧
-# @app.route('/events/<date>')
-# def events(date):
-#     from datetime import datetime
-#     selected_date = datetime.strptime(date, "%Y-%m-%d").date()
-#     events = Event.query.filter_by(date=selected_date).all()
-#     return render_template('events.html', date=selected_date, events=events)
+@Calen.route('/events/<date>')
+def events(date):
+    from datetime import datetime
+    selected_date = datetime.strptime(date, "%Y-%m-%d").date()
+    events = Calendar.query.filter_by(date=selected_date).all()
+    return render_template('event_list.html', 
+                           date=selected_date, events=events)
+
+
+# 選択された年月のカレンダーを表示
+@Calen.route('/select/<int:year>/<int:month>')
+def selectindex(year, month):
+    year = year
+    month = month
+
+    # カレンダーを作成
+    cal = calendar.Calendar(firstweekday=6)
+    month_days = cal.monthdayscalendar(year, month)
+
+    return render_template('calendar/calendar.html', year=year, month=month, month_days=month_days)
