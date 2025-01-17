5562284fd5fa8295e9e6eded1df9c615d350712b
username <mail@address.com>
2025-01-17 14:20:58 +0900

    変更戻し

--- minimalapp/translation/views.py
+++ minimalapp/translation/views.py
@@ -2,13 +2,13 @@ from flask import Blueprint
 
 
 # Blueprintで言語選択アプリを生成
-translation = Blueprint(
-    "translation",
+language = Blueprint(
+    "language",
     __name__,
     template_folder="templates",
     static_folder="../static"
 )
 
-@translation.route("/")
-def index():
-    return "Hello world"
+# @translation.route("/")
+# def index():
+#     return "Hello world"
