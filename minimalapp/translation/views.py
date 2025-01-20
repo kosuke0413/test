5bf576383edcdad4cc95c8994b244348fa74e95c
username <mail@address.com>
2025-01-20 12:17:58 +0900

    日本語と英語の言語変更可

--- minimalapp/translation/views.py
+++ minimalapp/translation/views.py
@@ -1,28 +1,3 @@
-# from flask import Blueprint, render_template
-# from minimalapp.tags.models import Local
-# from flask_login import current_user
-
-# # Blueprintで言語選択アプリを生成
-# translation = Blueprint(
-#     "translation",
-#     __name__,
-#     template_folder="templates",
-#     static_folder="../static"
-# )
-
-
-# @translation.route("/translation")
-# def index():
-#     return render_template("translation/translation.html")
-
-
-# # それぞれのパスに必要なやつ
-# @translation.context_processor
-# def inject_local():
-#     local = Local.query.get(current_user.local_id)
-#     return {
-#         "local": {"local_name": local.local_name}
-#     }
 from flask import Blueprint, render_template, request
 from minimalapp.tags.models import Local
 from flask_login import current_user
@@ -41,12 +16,13 @@ translation = Blueprint(
 def index():
     translated_text = ""
     if request.method == "POST":
-        # 入力されたテキストを取得
+        # 入力されたテキストとターゲット言語を取得
         text_to_translate = request.form.get("text_to_translate")
-        if text_to_translate:
+        target_language = request.form.get("target_language")
+        if text_to_translate and target_language:
             # 翻訳を実行
             translator = Translator()
-            translated = translator.translate(text_to_translate, dest="en")
+            translated = translator.translate(text_to_translate, dest=target_language)
             translated_text = translated.text
     return render_template("translation/translation.html",
                            translated_text=translated_text)
